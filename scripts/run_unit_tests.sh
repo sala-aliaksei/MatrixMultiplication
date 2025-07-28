237e93de074de977cd358fac468a7f9570f02f13
saloalexey <salo.alexey@gmail.com>
2025-07-27 21:09:16 -0700

    Add MATRIX_DIM env variable to run_unit_test

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -7,6 +7,14 @@ else
     TEST_NAME=$1
 fi
 
+
+if [ -z "$2" ]; then
+    Matrix_Size="2880"
+else
+    Matrix_Size=$2
+fi
+
+
 WORKSPACE=$(realpath $(dirname $0)/..)
 
-${WORKSPACE}/build/UT_Matmul --gtest_filter=$TEST_NAME
+MATRIX_DIM=$Matrix_Size ${WORKSPACE}/build/UT_Matmul --gtest_filter=$TEST_NAME --gtest_output=xml:${WORKSPACE}/build/results/UT_Matmul.xml
