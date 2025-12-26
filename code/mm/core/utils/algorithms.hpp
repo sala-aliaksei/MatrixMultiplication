80a217abeeae8c6cf03f4286ac61a8f599935416
Aliaksei Sala <salo.alexey@gmail.com>
2025-12-25 20:30:46 -0800

    Fix linter error

--- code/mm/core/utils/algorithms.hpp
+++ code/mm/core/utils/algorithms.hpp
@@ -8,5 +8,5 @@ template<long unsigned int N, class F>
 __attribute__((always_inline)) static constexpr void static_for(F&& f)
 {
     [&]<std::size_t... I>(std::index_sequence<I...>)
-    { (..., f.template operator()<I>()); }(std::make_index_sequence<N>{});
+    { (..., std::forward<F>(f).template operator()<I>()); }(std::make_index_sequence<N>{});
 }
