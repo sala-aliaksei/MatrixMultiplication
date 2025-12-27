15caa2a1c0cc46832cefe2dfd40c6695aeff91be
Aliaksei Sala <salo.alexey@gmail.com>
2025-12-26 21:32:18 -0800

    Update kernel benchmark

--- code/benchmarks/kernelsBenchmarks.cpp
+++ code/benchmarks/kernelsBenchmarks.cpp
@@ -1,27 +1,19 @@
 #include "mm/core/Matrix.hpp"
 #include "mm/core/kernels.hpp"
+#include "mm/core/zen5kernels.hpp"
+#include "mm/matmul/zen5_constants.hpp"
 #include <benchmark/benchmark.h>
 
-constexpr std::size_t ITER_NUM  = 1;
-benchmark::TimeUnit   TIME_UNIT = benchmark::kMillisecond;
-#define REGISTER(NAME, DIM) benchmark::RegisterBenchmark(#NAME, NAME)->Arg(DIM);
 
-constexpr std::size_t NN = 4 * 720;
-constexpr std::size_t M  = NN;
-constexpr std::size_t N  = NN; // + 8;
-constexpr std::size_t K  = NN;
 
-constexpr int Mc = 20;
-constexpr int Nc = 720;
 constexpr int Kc = 80;
 
-constexpr int Nr = 8;
-constexpr int Mr = 4;
 
 static void BM_CppGenericKern(benchmark::State& state)
 {
     std::size_t N        = state.range(0);
-    auto        matrices = initDoubleMatrix(M, N, K);
+    std::size_t K        = N;
+    auto        matrices = initDoubleMatrix(N, N, N);
 
     for (auto _ : state)
     {
@@ -34,7 +26,7 @@ static void BM_CppGenericKern(benchmark::State& state)
 static void BM_PackedKernelGeneric12x4(benchmark::State& state)
 {
     std::size_t N        = state.range(0);
-    auto        matrices = initDoubleMatrix(M, N, K);
+    auto        matrices = initDoubleMatrix(N, N, N);
 
     for (auto _ : state)
     {
@@ -47,7 +39,8 @@ static void BM_PackedKernelGeneric12x4(benchmark::State& state)
 static void BM_GenericKernel8x4(benchmark::State& state)
 {
     std::size_t N        = state.range(0);
-    auto        matrices = initDoubleMatrix(M, N, K);
+    std::size_t K        = N;
+    auto        matrices = initDoubleMatrix(N, N, N);
 
     for (auto _ : state)
     {
@@ -60,7 +53,7 @@ static void BM_GenericKernel8x4(benchmark::State& state)
 static void BM_PackedKernel8x4(benchmark::State& state)
 {
     std::size_t N        = state.range(0);
-    auto        matrices = initDoubleMatrix(M, N, K);
+    auto        matrices = initDoubleMatrix(N, N, N);
 
     for (auto _ : state)
     {
@@ -72,7 +65,7 @@ static void BM_PackedKernel8x4(benchmark::State& state)
 static void BM_PackedKernel8x4Aregs(benchmark::State& state)
 {
     std::size_t N        = state.range(0);
-    auto        matrices = initDoubleMatrix(M, N, K);
+    auto        matrices = initDoubleMatrix(N, N, N);
 
     for (auto _ : state)
     {
@@ -85,7 +78,7 @@ static void BM_PackedKernel8x4Aregs(benchmark::State& state)
 static void BM_PackedKernelGeneric8x4(benchmark::State& state)
 {
     std::size_t N        = state.range(0);
-    auto        matrices = initDoubleMatrix(M, N, K);
+    auto        matrices = initDoubleMatrix(N, N, N);
 
     for (auto _ : state)
     {
@@ -98,7 +91,7 @@ static void BM_PackedKernelGeneric8x4(benchmark::State& state)
 static void BM_PackedKernelGeneric4x4(benchmark::State& state)
 {
     std::size_t N        = state.range(0);
-    auto        matrices = initDoubleMatrix(M, N, K);
+    auto        matrices = initDoubleMatrix(N, N, N);
 
     for (auto _ : state)
     {
@@ -111,7 +104,7 @@ static void BM_PackedKernelGeneric4x4(benchmark::State& state)
 static void BM_PackedKernel4x4(benchmark::State& state)
 {
     std::size_t N        = state.range(0);
-    auto        matrices = initDoubleMatrix(M, N, K);
+    auto        matrices = initDoubleMatrix(N, N, N);
 
     for (auto _ : state)
     {
@@ -128,7 +121,9 @@ static void BM_GenericKernel2x4(benchmark::State& state)
     constexpr int Mr = 4;
 
     std::size_t N        = state.range(0);
-    auto        matrices = initDoubleMatrix(M, N, K);
+    std::size_t K        = N;
+
+    auto        matrices = initDoubleMatrix(N, N, N);
 
     for (auto _ : state)
     {
@@ -139,8 +134,9 @@ static void BM_GenericKernel2x4(benchmark::State& state)
 
 static void BM_PackedKernel2x4(benchmark::State& state)
 {
+    constexpr int Nr = 2;
     std::size_t N        = state.range(0);
-    auto        matrices = initDoubleMatrix(M, N, K);
+    auto        matrices = initDoubleMatrix(N, N, N);
 
     for (auto _ : state)
     {
@@ -150,8 +146,9 @@ static void BM_PackedKernel2x4(benchmark::State& state)
 
 static void BM_PackedKernelGeneric2x4(benchmark::State& state)
 {
+    constexpr int Nr = 2;
     std::size_t N        = state.range(0);
-    auto        matrices = initDoubleMatrix(M, N, K);
+    auto        matrices = initDoubleMatrix(N, N, N);
 
     for (auto _ : state)
     {
@@ -162,8 +159,9 @@ static void BM_PackedKernelGeneric2x4(benchmark::State& state)
 
 static void BM_PackedKernel6x4(benchmark::State& state)
 {
+    constexpr int Nr = 6;
     std::size_t N        = state.range(0);
-    auto        matrices = initDoubleMatrix(M, N, K);
+    auto        matrices = initDoubleMatrix(N, N, N);
 
     for (auto _ : state)
     {
@@ -173,8 +171,9 @@ static void BM_PackedKernel6x4(benchmark::State& state)
 
 static void BM_PackedKernel1x4(benchmark::State& state)
 {
+    constexpr int Nr = 1;
     std::size_t N        = state.range(0);
-    auto        matrices = initDoubleMatrix(M, N, K);
+    auto        matrices = initDoubleMatrix(N, N, N);
 
     for (auto _ : state)
     {
@@ -185,39 +184,133 @@ static void BM_PackedKernel1x4(benchmark::State& state)
 static void BM_PackedKernel1x4_Simd(benchmark::State& state)
 {
     std::size_t N        = state.range(0);
-    auto        matrices = initDoubleMatrix(M, N, K);
+    auto        matrices = initDoubleMatrix(N, N, N);
 
     for (auto _ : state)
     {
         kernels::packed_ukernel1x4_simd<Kc>(
-          matrices.a.data(), matrices.b.data(), matrices.c.data(), Nr);
+          matrices.a.data(), matrices.b.data(), matrices.c.data(), N);
     }
 }
 
-int main(int argc, char** argv)
+static void BM_NaiveBlockDynamicKc(benchmark::State& state)
 {
-    int matrix_dim = NN;
+    std::size_t memBytes = state.range(0);
+    constexpr std::size_t Mr = mm::constants::MatMulZen5Config<double>::Mr;
+    constexpr std::size_t Nr = mm::constants::MatMulZen5Config<double>::Nr;
+    constexpr std::size_t Nc = mm::constants::MatMulZen5Config<double>::Nc;
+    constexpr std::size_t Mc = mm::constants::MatMulZen5Config<double>::Mc;
 
-    REGISTER(BM_CppGenericKern, matrix_dim);
-    REGISTER(BM_PackedKernelGeneric12x4, matrix_dim);
-    REGISTER(BM_GenericKernel8x4, matrix_dim);
-    REGISTER(BM_PackedKernel8x4, matrix_dim);
-    REGISTER(BM_PackedKernel8x4Aregs, matrix_dim);
-    REGISTER(BM_PackedKernelGeneric8x4, matrix_dim);
 
-    REGISTER(BM_PackedKernelGeneric4x4, matrix_dim);
-    REGISTER(BM_PackedKernel4x4, matrix_dim);
+    int Kc = memBytes / (Mr + Nr) / sizeof(double);
 
-    REGISTER(BM_GenericKernel2x4, matrix_dim);
-    REGISTER(BM_PackedKernel2x4, matrix_dim);
-    REGISTER(BM_PackedKernelGeneric2x4, matrix_dim);
-    REGISTER(BM_PackedKernel6x4, matrix_dim);
+    auto          matrices = initDoubleMatrix(Mc, Nc, Kc);
 
-    REGISTER(BM_PackedKernel1x4, matrix_dim);
-    REGISTER(BM_PackedKernel1x4_Simd, matrix_dim);
+    for (auto _ : state)
+    {
+        kernels::naive_block_dkc<Nr, Mr>(matrices.a.data(), matrices.b.data(), matrices.c.data(), Nc, Kc);
+        benchmark::ClobberMemory();
+    }
 
-    benchmark::Initialize(&argc, argv);
-    benchmark::RunSpecifiedBenchmarks();
+    std::size_t bytes_read = (Kc * Mr + Kc * Nr + Mr * Nr) * sizeof(double);
+    std::size_t bytes_written = Mr * Nr * sizeof(double);
+    std::size_t total_bytes = bytes_read + bytes_written;
 
-    return 0;
+    state.counters["FLOPS"] = benchmark::Counter(2*Kc * Mr * Nr * state.iterations(), benchmark::Counter::kIsRate);
+    state.counters["MemBW"] = benchmark::Counter(total_bytes * state.iterations(), benchmark::Counter::kIsRate);
 }
+
+
+static void BM_PackedKernelZen5(benchmark::State& state)
+{
+    std::size_t memBytes = state.range(0);
+    constexpr std::size_t Nc = 96;
+    constexpr std::size_t Mc = 96;
+    constexpr std::size_t Mr = 8;
+    constexpr std::size_t Nr = 24;
+    int Kc = memBytes / (Mr + Nr) / sizeof(double);
+
+    auto          matrices = initDoubleMatrix(Mc,Nc,Kc);
+
+    for (auto _ : state)
+    {
+        kernels::zen5_packed_kernel<Nr, Mr>(
+          matrices.a.data(), matrices.b.data(), matrices.c.data(), Nc, Kc);
+
+        benchmark::ClobberMemory();
+    }
+
+    std::size_t bytes_read = (Kc * Mr + Kc * Nr + Mr * Nr) * sizeof(double);
+    std::size_t bytes_written = Mr * Nr * sizeof(double);
+    std::size_t total_bytes = bytes_read + bytes_written;
+
+    state.counters["FLOPS"] = benchmark::Counter(2*Kc * Mr * Nr * state.iterations(), benchmark::Counter::kIsRate);
+    state.counters["MemBW"] = benchmark::Counter(total_bytes * state.iterations(), benchmark::Counter::kIsRate);
+}
+
+
+BENCHMARK(BM_NaiveBlockDynamicKc)
+  ->Arg(4 * 1024)
+  ->Arg(8 * 1024)
+  ->Arg(16 * 1024)
+  ->Arg(24 * 1024)
+  ->Arg(48 * 1024) // L1 Cache size (9950x)
+  ->Arg(52 * 1024)
+  ->Arg(64 * 1024)
+  ->Arg(72 * 1024)
+  ->Arg(84 * 1024)
+  ->Arg(96 * 1024)
+  ->Arg(256 * 1024)
+  ->Arg(384 * 1024)
+  ->Arg(512 * 1024)
+  ->Arg(640 * 1024)
+  ->Arg(768 * 1024)
+  ->Arg(896 * 1024)
+  ->Arg(1024 * 1024) // L2 Cache size(9950x)
+  ->Arg((256 + 1024) * 1024)
+  ->Arg((512 + 1024) * 1024)
+  ->Arg((768 + 1024) * 1024)
+  ->Arg(2 * 1024 * 1024)
+  ->Arg(4 * 1024 * 1024) // L3 Cache size(9950x)
+  ->Arg(8 * 1024 * 1024)
+  ->Arg(16 * 1024 * 1024)
+  ->Arg(20 * 1024 * 1024)
+  ->Arg(24 * 1024 * 1024)
+  ->Arg(32 * 1024 * 1024)
+  ->Arg(48 * 1024 * 1024)
+  ->Arg(64 * 1024 * 1024)
+  ->Threads(1);
+
+BENCHMARK(BM_PackedKernelZen5)
+  ->Arg(4 * 1024)
+  ->Arg(8 * 1024)
+  ->Arg(16 * 1024)
+  ->Arg(24 * 1024)
+  ->Arg(48 * 1024) // L1 Cache size (9950x)
+  ->Arg(52 * 1024)
+  ->Arg(64 * 1024)
+  ->Arg(72 * 1024)
+  ->Arg(84 * 1024)
+  ->Arg(96 * 1024)
+  ->Arg(256 * 1024)
+  ->Arg(384 * 1024)
+  ->Arg(512 * 1024)
+  ->Arg(640 * 1024)
+  ->Arg(768 * 1024)
+  ->Arg(896 * 1024)
+  ->Arg(1024 * 1024) // L2 Cache size(9950x)
+  ->Arg((256 + 1024) * 1024)
+  ->Arg((512 + 1024) * 1024)
+  ->Arg((768 + 1024) * 1024)
+  ->Arg(2 * 1024 * 1024)
+  ->Arg(4 * 1024 * 1024) // L3 Cache size(9950x)
+  ->Arg(8 * 1024 * 1024)
+  ->Arg(16 * 1024 * 1024)
+  ->Arg(20 * 1024 * 1024)
+  ->Arg(24 * 1024 * 1024)
+  ->Arg(32 * 1024 * 1024)
+  ->Arg(48 * 1024 * 1024)
+  ->Arg(64 * 1024 * 1024)
+  ->Threads(1);
+
+  BENCHMARK_MAIN();
